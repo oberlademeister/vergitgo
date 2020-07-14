f476d546a02204890f734d9d7850ce8ee4f85b57
Holger Fuessler <holger@fuessler.eu>
2020-07-14 09:03:16 +0200

    bumping version

--- genautoversion.go
+++ genautoversion.go
@@ -3,7 +3,7 @@ package main
 // This file is created by hfgit, v2-1-g67cdbf8-dirty hf@hoffamac.local 2017-07-28T13:51:36+02:00
 
 // VersionInfo contains the version information
-const VersionInfo = "872eebd"
+const VersionInfo = "v1.0.0-0-g51b3d4a"
 
 // BuildInfo contains the information about the go generate run
-const BuildInfo = "hf@mm 2020-07-14T08:53:56+02:00"
+const BuildInfo = "hf@mm 2020-07-14T09:02:41+02:00"
